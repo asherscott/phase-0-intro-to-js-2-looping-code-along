c13b113b7665657ff4908fecf6ab8a283fc7c790
asher scott <asher.scott6@gmail.com>
2022-02-25 10:32:07 -0600

    commit tree 980be42950a4d69ba41f2566f29bbb3c9b4cb921

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array) {
+    let thk = [];
+    for(const ele in array) {
+        thk[ele] = `Thank you, ${array[ele]}, for the wonderful surprise gift!`;
+    }
+    return thk;
+}
+
+function countDown(num) {
+    let num1 = num;
+    for(let i = 0; i <= num; i++) {
+        console.log(num1);
+        num1 = num1 - 1;
+    }
+}
